b80729b4339607d2287ee15da2ca80b47242641b
lvoinescu <voinescu_lucian@yahoo.com>
2016-09-15 22:52:27 +0300

    Change icon for disable/enable dots.

--- DrawingApplication/MainForm.Designer.cs
+++ DrawingApplication/MainForm.Designer.cs
@@ -74,7 +74,6 @@ namespace DrawingApplication
 			this.dotDrawing = new DeltaDrawing.DotDrawing.DotDrawing();
 			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
 			this.toolStripButton8 = new System.Windows.Forms.ToolStripButton();
-			this.toolStripButton9 = new System.Windows.Forms.ToolStripButton();
 			this.toolStripSeparator4 = new System.Windows.Forms.ToolStripButton();
 			this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
 			this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
@@ -83,6 +82,7 @@ namespace DrawingApplication
 			this.toolStripButton3 = new System.Windows.Forms.ToolStripButton();
 			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
 			this.snapToGridButton = new System.Windows.Forms.ToolStripButton();
+			this.toolStripButton9 = new System.Windows.Forms.ToolStripButton();
 			this.toolStripButton7 = new System.Windows.Forms.ToolStripButton();
 			this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
 			this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
@@ -223,6 +223,16 @@ namespace DrawingApplication
 			this.snapToGridButton.Text = "Snap to grid";
 			this.snapToGridButton.Click += new System.EventHandler(this.ToolStripButton4Click);
 			// 
+			// toolStripButton9
+			// 
+			this.toolStripButton9.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+			this.toolStripButton9.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton9.Image")));
+			this.toolStripButton9.ImageTransparentColor = System.Drawing.Color.Magenta;
+			this.toolStripButton9.Name = "toolStripButton9";
+			this.toolStripButton9.Size = new System.Drawing.Size(23, 22);
+			this.toolStripButton9.Text = "Disable points";
+			this.toolStripButton9.Click += new System.EventHandler(this.ToolStripButton9Click);
+			// 
 			// toolStripButton7
 			// 
 			this.toolStripButton7.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
@@ -317,18 +327,6 @@ namespace DrawingApplication
 			this.toolStripAppCloseButton.Size = new System.Drawing.Size(91, 22);
 			this.toolStripAppCloseButton.Text = "Close application";
 			this.toolStripAppCloseButton.Click += new System.EventHandler(this.ToolStripAppCloseButton);
-
-			// 
-			// toolStripButton8
-			// 
-			this.toolStripButton9.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.toolStripButton9.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton8.Image")));
-			this.toolStripButton9.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.toolStripButton9.Name = "toolStripButton9";
-			this.toolStripButton9.Size = new System.Drawing.Size(23, 22);
-			this.toolStripButton9.Text = "Disable points";
-			this.toolStripButton9.Click += new System.EventHandler(this.ToolStripButton9Click);
-
 			// 
 			// MainForm
 			// 
